3f885e394c021a1f0b030b80798c64e7f61e8a20
Wotipati <k940805k.lab@gmail.com>
2018-05-03 20:32:00 +0900

    minor update

--- passive_aggressive_ex.py
+++ passive_aggressive_ex.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.animation as animation
+import matplotlib.gridspec as gridspec
 from simple_dataset import SimpleDataset
 from passive_aggressive import PassiveAggressive
 
@@ -28,50 +30,60 @@ class PassiveAggressiveTwo(PassiveAggressive):
 
 def main():
     train_dataset = SimpleDataset(total_num=1000, is_confused=True,  x=3, y=5, seed=1)
-    test_dataset  = SimpleDataset(total_num=100 , is_confused=False, x=3, y=5, seed=2)
+    test_dataset  = SimpleDataset(total_num=250 , is_confused=False, x=3, y=5, seed=2)
 
-    model_PA  = PassiveAggressive()
-    model_one = PassiveAggressiveOne(0.1)
-    model_two = PassiveAggressiveTwo(0.1)
+    model_PA     = PassiveAggressive()
+    model_PA_one = PassiveAggressiveOne(0.05)
 
-    plt.style.use('seaborn-colorblind')
-    plt.xlim([train_dataset.dataset.x1.min() - 0.1, train_dataset.dataset.x1.max() + 0.1])
-    plt.ylim([train_dataset.dataset.x2.min() - 0.1, train_dataset.dataset.x2.max() + 0.1])
+    fig = plt.figure(figsize=(17, 5))
+    gs = gridspec.GridSpec(1,10)
+    fig_left  = fig.add_subplot(gs[0,:3])
+    fig_right = fig.add_subplot(gs[0,4:])
+ 
+    #plt.style.use('seaborn-colorblind')
+    fig_left.set_xlim([train_dataset.dataset.x1.min() - 0.1, train_dataset.dataset.x1.max() + 0.1])
+    fig_left.set_ylim([train_dataset.dataset.x2.min() - 0.1, train_dataset.dataset.x2.max() + 0.1])
+    
+    fig_right.set_xlim([0, len(train_dataset.y)])
+    fig_right.set_ylim([0,1])
+    fig_right.set_title("Accuracy")
 
     line_x = np.array(range(-10, 10, 1))
     line_y = line_x*0
-    line_PA,     = plt.plot(line_x, line_y, c="#2980b9", label="PA")
-    line_PA_one, = plt.plot(line_x, line_y, c="#e74c3c", label="PA-1")
-    line_PA_two, = plt.plot(line_x, line_y, c="#f1c40f", label="PA-2")
+    line_PA,     = fig_left.plot(line_x, line_y, c="#2980b9", label="PA"  )
+    line_PA_one, = fig_left.plot(line_x, line_y, c="#e74c3c", label="PA-1")
+
+    fig_left.legend(handles=[line_PA, line_PA_one])
+    fig_right.legend(handles=[line_PA, line_PA_one])
     
     valid_result_sample = []
     accuracies_PA = []
+    accuracies_PA_one = []
+    imgs = []
     
+    #for i in range(20):
     for i in range(len(train_dataset.y)):
-        plt.scatter(x=train_dataset.dataset.x1[i], y=train_dataset.dataset.x2[i], c=cm.cool(train_dataset.dataset.label[i]), alpha=0.5)
+        fig_left.scatter(x=train_dataset.dataset.x1[i], y=train_dataset.dataset.x2[i], c=cm.cool(train_dataset.dataset.label[i]), alpha=0.5)
+        
         model_PA.fit(train_dataset.feature_vec[i], train_dataset.y[i])
-        model_one.fit(train_dataset.feature_vec[i], train_dataset.y[i])
-        model_two.fit(train_dataset.feature_vec[i], train_dataset.y[i])
+        model_PA_one.fit(train_dataset.feature_vec[i], train_dataset.y[i])
             
-        # print(test_dataset.valid_training_result(model_PA))
-        # valid_result_sample.append(i)
-        # accuracies_PA.append(test_dataset.valid_training_result(model_PA))
+        accuracies_PA.append(test_dataset.valid_training_result(model_PA))
+        accuracies_PA_one.append(test_dataset.valid_training_result(model_PA_one))
         
         a, b, c = model_PA.w
         line_y = (a*line_x + c)/(-b)
         line_PA.set_data(line_x, line_y)
         
-        a, b, c = model_one.w
+        a, b, c = model_PA_one.w
         line_y = (a*line_x + c)/(-b)
         line_PA_one.set_data(line_x, line_y)
         
-        a, b, c = model_two.w
-        line_y = (a*line_x + c)/(-b)
-        line_PA_two.set_data(line_x, line_y)
+        fig_right.plot(accuracies_PA    , c="#2980b9", label="PA"  )
+        fig_right.plot(accuracies_PA_one, c="#e74c3c", label="PA-1")
         
-        plt.legend(handles=[line_PA, line_PA_one, line_PA_two])
         plt.pause(0.005)
-
+        
 
 if __name__ == '__main__':
     main()
